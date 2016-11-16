7b495fc74383374e9cc77da5d63809fda012a275
Gregory McGrath <gpmcgrath@umass.edu>
2016-11-16 16:05:31 -0500

    added allItems to feed.js

--- app/components/feed.js
+++ app/components/feed.js
@@ -19,18 +19,28 @@ export default class Feed extends React.Component{
     this.refresh();
   }
   render(){
+    var allItems = this.state.items.map((item) => {
+              console.log("here2 " +item);
+              return (
+                <Item key={item._id} data={item} />
+              );
+            })
+    console.log(allItems);
+    var item
+    var text
+    if(allItems.length > 0){
+      item = allItems[0].props.data.contents;
+    }
+    else{
+      text = "No More Items";
+    }
     return(
       <div>
         <Link to={"/profile/5"}>
           TEST FEED
         </Link>
-        {console.log("now" + this.state.items)}
-        {this.state.items.map((item) => {
-          console.log("here2 " +item);
-          return (
-            <Item key={item._id} data={item} />
-          );
-        })}
+        <img src={item}/>
+        <h1>{text}</h1>
       </div>
     )
   }
