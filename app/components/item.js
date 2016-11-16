05bb67d1bcbfd6fbf9c7b91caf1a44d4561c213e
Gregory McGrath <gpmcgrath@umass.edu>
2016-11-16 07:57:41 -0500

    worked on item.js, still some bugs

--- app/components/item.js
+++ app/components/item.js
@@ -1,12 +1,40 @@
-
 import React from 'react';
 
 export default class Item extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = props.data;
+  }
   render(){
+    var data;
+    data = (
+      <Item key = {this.state._id}
+        name = {this.state.name}
+        category = {this.state.category}
+        contents = {this.state.contents}
+        >
+      </Item>
+    )
     return(
       <div>
-        Hello
-      </div>
+        <ul>
+          <ul>
+            <div>Name: {this.state.name}</div>
+          </ul>
+
+          <ul>
+            <div>Description: {this.state.description}</div>
+          </ul>
+
+          <ul>
+            <div>Category: {this.state.category}</div>
+          </ul>
+
+          <ul>
+            <img className="img-responsive" src={this.state.contents} />
+          </ul>
+        </ul>
+    </div>
     )
   }
 
