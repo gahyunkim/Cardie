12422100b58a7633e49bf71a8c8d105ed9ec9928
Gregory McGrath <gpmcgrath@umass.edu>
2016-11-16 08:16:10 -0500

    added refresh method to feed.js

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,13 +1,18 @@
-
 import React from 'react';
 import Item from './item.js';
 import {Link} from 'react-router';
+import {getFeedData} from '../server';
 
 export default class Feed extends React.Component{
   constructor(props) {
       super(props);
       this.state = props.data;
     }
+  refresh(){
+    getFeedData(5, (feedData)=>{
+      this.setState(feedData);
+    });
+  }
   render(){
     return(
       <div>
