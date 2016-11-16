2b84b0416a592007cd884bf09f6515d60bb3b920
Gregory McGrath <gpmcgrath@umass.edu>
2016-11-16 08:37:01 -0500

    added componentDidMount and added more to render in feed.js

--- app/components/feed.js
+++ app/components/feed.js
@@ -5,13 +5,18 @@ import {getFeedData} from '../server';
 
 export default class Feed extends React.Component{
   constructor(props) {
-      super(props);
-      this.state = props.data;
-    }
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
   refresh(){
     getFeedData(5, (feedData)=>{
       this.setState(feedData);
     });
+  }
+  componentDidMount() {
+    this.refresh();
   }
   render(){
     return(
@@ -19,6 +24,13 @@ export default class Feed extends React.Component{
         <Link to={"/profile/5"}>
           TEST FEED
         </Link>
+        {console.log("now" + this.state.items)}
+        {this.state.items.map((item) => {
+          console.log("here2 " +item);
+          return (
+            <Item key={item._id} data={item} />
+          );
+        })}
       </div>
     )
   }
