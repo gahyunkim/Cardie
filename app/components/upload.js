6dcd1a8d51388df93c2a1d9e05d59b8f5d556f54
daanialahmed <d_ahmed100@yahoo.com>
2016-11-16 21:12:41 -0500

    just style changes

--- app/components/upload.js
+++ app/components/upload.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {getUserProfile} from '../server';
+import Item from './item.js';
+
 
 export default class Upload extends React.Component{
   constructor(props) {
@@ -22,7 +24,10 @@ export default class Upload extends React.Component{
                         <img src="/img/camera-solid.jpg" alt="..." className="img-thumbnail imagesize" />
                     </div>
                     <div className="col-md-3"><button className="btn btn-default btn-lg buttonsize" type="submit">Choose <span className="glyphicon glyphicon-camera" aria-hidden="true"></span></button></div>
-                    <div className="col-md-3 categoriesbutton"><select className="form-control input-lg categoriesbutton">
+                      <div className="col-md-3 titlebox">
+                          <textarea className="form-control" rows="1" placeholder="Title"></textarea>
+                      </div>
+                  <div className="col-md-3 categoriesbutton"><select className="form-control input-lg categoriesbutton">
                       <option selected disabled>Category</option>
                       <option>Auto</option>
                       <option>Electronics</option>
@@ -35,7 +40,7 @@ export default class Upload extends React.Component{
                       <option>Other</option>
                     </select></div>
                     <div className="col-md-3 descriptionbox">
-                        <textarea className="form-control" rows="7" placeholder="Enter Description"></textarea>
+                        <textarea className="form-control" rows="7" placeholder="Description"></textarea>
                     </div>
 
                 </div>
@@ -44,8 +49,8 @@ export default class Upload extends React.Component{
                         <p>Item expires: </p>
                         <input type="date" className="form-control expireform" placeholder="Text input" />
                     </div>
-                      <div><div className="col-md-2"><button className="btn btn-default btn-lg submitsubmitbutton btn-success" type="submit">Upload <span className="glyphicon glyphicon-upload" aria-hidden="true"></span></button></div></div>
-                      <div><div className="col-md-2"><button className="btn btn-default btn-lg submitcancelbutton btn-danger" type="submit">Cancel</button></div></div>
+                      <div className="col-md-2"><button className="btn btn-default btn-lg submitsubmitbutton btn-success" type="submit">Upload <span className="glyphicon glyphicon-upload" aria-hidden="true"></span></button></div>
+                      <div className="col-md-2"><button className="btn btn-default btn-lg submitcancelbutton btn-danger" type="submit">Cancel</button></div>
                     </div>
 
             </div>
