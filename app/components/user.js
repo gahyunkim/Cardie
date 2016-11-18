d5dc4fd774185bbef873ec03dccb5b7dd8fe9b26
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-11-17 22:58:14 -0500

    Simplified user page, added My Info block

--- app/components/user.js
+++ app/components/user.js
@@ -11,34 +11,11 @@ export default class User extends React.Component{
     this.state = props.data
   }
 
-  handleUserDescription(e) {
-    e.preventDefault();
-    var userDescriptionText = this.state.value.trim();
-    if (userDescriptionText !== "") {
-      createUserDescription(e, this.props.user, (updatedUserDescription) =>
-      {this.setState(updatedUserDescription)})
-    }
-  }
-
-  /**
-  * Called when the user types a character into the status update box.
-  * @param e An Event object.
-  */
-  handleChange(e) {
-    // Prevent the event from "bubbling" up the DOM tree.
-    e.preventDefault();
-    // e.target is the React Virtual DOM target of the
-    // input event -- the <textarea> element. The textarea's
-    // `value` is the entire contents of what the user has
-    // typed in so far.
-    this.setState({value: e.target.value});
-  }
-
   render(){
     var userData = getUserProfile(this.props.user);
     return(
       <div>
-        <h1>{"Hi, I'm " + userData.username}</h1>
+        <h1>{"Hello, " + userData.username}</h1>
         <div>
           <div className="container">
             <div className="row">
@@ -49,84 +26,40 @@ export default class User extends React.Component{
                     <div>
                       <div className="row">
                         <div className="col-md-6">
-                          <div className="media">
-                            <div className="media-body">
-                              <font size="+1" color="black">Description</font>
-                            </div>
-                          </div>
+                          <font size="+2" color="black">My Info</font>
                         </div>
                       </div>
                       <div className="row">
-                        <div className="col-md-12 description">
-                          <div className="media-body">
-                            <font size="+0.5" color="black">{this.props.description}</font>
-                          </div>
-                          <div className="description">
-                          <textarea className="form-control"
-                            rows="2"
-                            placeholder= "Add a description here!" />
-                        </div>
-                      </div>
-                      </div>
-                    </div>
-
-
-                <div className="row profile-update">
-                  <div className="col-md-12">
-                    <div className="media">
-                      <div className="media-body description">
-                        <font size="+1" color="black">Skills</font>
-                          <div className="media-body">
-                            <font size="+0.5" color="black">{this.props.description}</font>
-                          </div>
-                          <div className="description">
-                          <textarea className="form-control"
-                            rows="2"
-                            placeholder= "Add a skill!"
-                            onChange={(e) => this.handleChange(e)} />
-                        </div>
-                      </div>
+                        <div className="col-md-12 ">
+                          <ul className="nav nav-pills nav-stacked accounts user-description">
+                            <li role="presentation">
+                              <a href="#">
+                                <span className="glyphicon glyphicon-sunglasses"></span>
+                                {userData.username}</a>
+                            </li>
+                            <li role="presentation">
+                              <a href="#">
+                                <span className="glyphicon glyphicon-user"></span>
+                                {userData.fullName}</a>
+                            </li>
+                            <li role="presentation">
+                              <a href="#"><span className="glyphicon glyphicon-envelope"></span>
+                              {userData.email}</a>
+                          </li>
+                          <li role="presentation">
+                            <a href="#"><span className="glyphicon glyphicon-home"></span>
+                            {userData.location}</a>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
 
-                <div className="row profile-update">
-                  <div className="col-md-12">
-                    <div className="media">
-                      <div className="media-body description">
-                        <font size="+1" color="black">Education</font>
-                          <div className="media-body">
-                            <font size="+0.5" color="black">{this.props.description}</font>
-                          </div>
-                          <div className="description">
-                          <textarea className="form-control"
-                            rows="2"
-                            placeholder= "Add a skill!"
-                            onChange={(e) => this.handleChange(e)} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
 
-                <div className="row profile-update">
-                  <div className="col-md-12">
-                    <div className="media">
-                      <div className="media-body description">
-                        <font size="+1" color="black">Certification</font>
-                          <div className="media-body">
-                            <font size="+0.5" color="black">{this.props.description}</font>
-                          </div>
-                          <div className="description">
-                          <textarea className="form-control"
-                            rows="2"
-                            placeholder= "Add a skill!"
-                            onChange={(e) => this.handleChange(e)} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+
+
+
+
 
               </div>
             </div>
@@ -152,38 +85,6 @@ export default class User extends React.Component{
                           </div>
                         </div>
                       </div>
-                      <div className="row">
-                        <div className="col-md-6">
-                          <div className="media">
-                            <div className="media-body">
-                              <font size="+1" color="black">Information</font>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className="row profile-update">
-                        <div className="col-md-10">
-                          <div className="media">
-
-                            <ul className="nav nav-pills nav-stacked accounts user-description">
-                              <li role="presentation">
-                                <a href="#">
-                                  <span className="glyphicon glyphicon-user"></span>
-                                  {userData.fullName}</a>
-                              </li>
-                              <li role="presentation">
-                                <a href="#"><span className="glyphicon glyphicon-envelope"></span>
-                                {userData.email}</a>
-                            </li>
-                            <li role="presentation">
-                              <a href="#"><span className="glyphicon glyphicon-home"></span>
-                              {userData.location}</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
