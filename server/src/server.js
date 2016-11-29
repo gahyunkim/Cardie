8c9efcc1ad29f8bff340755be657bda1772897e9
Francis Phan <fphan@umass.edu>
2016-11-29 17:09:33 -0500

    Application now works on localhost:3000

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,36 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+app.use(express.static('../client/build'));
+
+// Support receiving text in HTTP request bodies
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
+var database = require('./database');
+var readDocument = database.readDocument;
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
