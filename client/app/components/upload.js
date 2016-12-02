d22e136d808fb4c07265699688d30106531522e8
daanialahmed <d_ahmed100@yahoo.com>
2016-12-01 22:04:10 -0500

    form control functions finished, still adding button functionality

--- client/app/components/upload.js
+++ client/app/components/upload.js
@@ -12,95 +12,95 @@ export default class Upload extends React.Component{
     };
   }
   /*** Called when the user clicks the 'post' button.
-* Triggers the `Upload` prop if the post isn't empty, and clears* the component.
-*/
+  * Triggers the `Upload` prop if the post isn't empty, and clears* the component.
+  */
   handleUpload(e) {
-
-  }
-
-  //Called when the user types a character into the form boxes.
-  handleItemName(e) {
     e.preventDefault();
-    var itemNameText = this.state.value.trim();
+    var itemNameText = this.state.namevalue.trim();
     if(itemNameText !== "") {
-      this.setState({value: ""});
-    }
-  }
-  handleCategory(e) {
-    e.preventDefault();
-    var itemCategory = this.state.value.trim();
-    if(itemCategory !== "") {
-      this.setState({value: ""});
-    }
-  }
-  handleItemDescrip(e) {
-    e.preventDefault();
-    var itemDescrip = this.state.value.trim();
-    if(itemDescrip !== "") {
-      this.setState({value: ""});
-    }
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+      this.setState({namevalue: ""});
 
-  previewImage() {
-    var preview = document.getElementById("myFile");
-    this.setState({photo: preview})
-  }
+      var itemCategory = this.state.categoryvalue.trim();
+      if(itemCategory !== "") {
+        this.setState({categoryvalue: ""});
+      }
 
-
-  render(){
-    // var userData = getUserProfile(5);
-    return(
-      <div>
-        <div className="panel panel-default">
-          <div className="panel-body">
-            <form>
-              <div className="row">
-                <div className="col-md-6">
-                  <img src="/img/camera-solid.jpg" alt="..." className="img-thumbnail imagesize" />
-                </div>
-                <form encType="multipart/form-data" action="/upload/image" method="post">
-                  <input id="myFile" type="file" onChange="previewImage()"/>
-                </form>
-                <div className="col-md-3 descriptionbox">
-                  <textarea className="form-control" rows="1" placeholder="Item Name"></textarea>
-                </div>
-                <div className="col-md-3 categoriesbutton"><select className="form-control input-lg categoriesbutton">
-                  <option>Category</option>
-                  <option>Auto</option>
-                  <option>Electronics</option>
-                  <option>Dorm</option>
-                  <option>Home & Furniture</option>
-                  <option>Sports & Outdoor</option>
-                  <option>TV, Movies, Books</option>
-                  <option>Food</option>
-                  <option>Supplies</option>
-                  <option>Other</option>
-                </select></div>
-                <div className="col-md-3 descriptionbox">
-                  <textarea className="form-control" rows="7" placeholder="Enter Description"></textarea>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-5 expirerow">
-                  <p>Item expires: </p>
-                  <input type="date" className="form-control expireform" placeholder="Text input" />
-                </div>
-                <div>
-                  <div className="col-md-2">
-                    <button className="btn btn-default btn-lg submitsubmitbutton btn-success" type="submit" onClick={(e) =>this.handlePost(e)}>
-                      Upload
-                      <span className="glyphicon glyphicon-upload" aria-hidden="true"></span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
+      var itemDescrip = this.state.descriptionvalue.trim();
+      if(itemDescrip !== "") {
+        this.setState({descriptionvalue: ""});
+    }
   }
 }
+
+    //Called when the user types a character into the form boxes.
+    handleItemName(e) {
+      this.setState({namevalue: e.target.value});
+    }
+    handleCategory(e) {
+      this.setState({categoryvalue: e.target.value});
+    }
+    handleItemDescrip(e) {
+      this.setState({descriptionvalue: e.target.value});
+    }
+
+
+    previewImage() {
+      var preview = document.getElementById("myFile");
+      this.setState({photo: preview})
+    }
+
+
+    render(){
+      // var userData = getUserProfile(5);
+      return(
+        <div>
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <form>
+                <div className="row">
+                  <div className="col-md-6">
+                    <img src="/img/camera-solid.jpg" alt="..." className="img-thumbnail imagesize" />
+                  </div>
+                  <form encType="multipart/form-data" action="/upload/image" method="post">
+                    <input id="myFile" type="file" onChange="previewImage()"/>
+                  </form>
+                  <div className="col-md-3 descriptionbox">
+                    <textarea className="form-control" rows="1" placeholder="Item Name"></textarea>
+                  </div>
+                  <div className="col-md-3 categoriesbutton"><select className="form-control input-lg categoriesbutton">
+                    <option>Category</option>
+                    <option>Auto</option>
+                    <option>Electronics</option>
+                    <option>Dorm</option>
+                    <option>Home & Furniture</option>
+                    <option>Sports & Outdoor</option>
+                    <option>TV, Movies, Books</option>
+                    <option>Food</option>
+                    <option>Supplies</option>
+                    <option>Other</option>
+                  </select></div>
+                  <div className="col-md-3 descriptionbox">
+                    <textarea className="form-control" rows="7" placeholder="Enter Description"></textarea>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-5 expirerow">
+                    <p>Item expires: </p>
+                    <input type="date" className="form-control expireform" placeholder="Text input" />
+                  </div>
+                  <div>
+                    <div className="col-md-2">
+                      <button className="btn btn-default btn-lg submitsubmitbutton btn-success" type="submit" onClick={(e) =>this.handlePost(e)}>
+                        Upload
+                        <span className="glyphicon glyphicon-upload" aria-hidden="true"></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
