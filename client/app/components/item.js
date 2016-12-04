d2711f82ca06dadce016b413e7ad0f3a6a954d71
Roman Ganchin <rganchin@umass.edu>
2016-12-03 20:07:44 -0500

    fixed trending so that clicking on images works, and fixed feed so that the button is the image

--- client/app/components/item.js
+++ client/app/components/item.js
@@ -16,12 +16,14 @@ export default class Item extends React.Component{
     this.refresh();
   }
   render(){
-    var item = this.state.data;
-    if(this.state.isCat === true){
+    console.log(this.state);
+    var item = this.state;
+    console.log(item);
+    if(item.isCat === true){
       return(
         <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-          <Link to={'/item/'+this.state.item._id}>
-            <img className="img-responsive" src={this.state.item.contents} />
+          <Link to={'/item/'+ item._id}>
+            <img className="img-responsive" src={item.item.contents} />
           </Link>
         </div>
       )
