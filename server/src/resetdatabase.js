bb664d59043b51f5d8e3c84125744d9ec19ea9d9
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-12-06 23:13:27 -0500

    Converted mock database IDs to MongoDB ObjectIDs and changed current Integer type to String in schema

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -2,7 +2,259 @@ var ObjectID = require('mongodb').ObjectID;
 
 var databaseName = "Cardie";
 // Put the initial mock objects here.
-var initialData = null;
+var initialData = {
+  // USERS
+  "users": {
+    "1" : {
+      "_id" : new ObjectID("000000000000000000000001"),
+      "nickname" : "phancis",
+      "fullName" : "Francis Phan",
+      "email" : "fphan@umass.edu",
+      "location" : "Amherst, MA",
+      "feed": 1,
+      "productManager" : {
+        "_id" : new ObjectID("000000000000000000000001"),
+        "items" : [1,5,6,7,8]
+      },
+      "messages" : {
+        "_id" : new ObjectID("000000000000000000000001"),
+        "messages" : [1, 2, 3]
+      }
+    },
+    "2" : {
+      "_id" : new ObjectID("000000000000000000000002"),
+      "nickname" : "rcui",
+      "fullName" : "Richard Cui",
+      "email" : "rcui@umass.edu",
+      "location" : "Amherst, MA",
+      "feed": 2,
+      "productManager" : {
+        "_id" : 2,
+        "items" : []
+      },
+      "messages" : {
+        "_id" : 2,
+        "messages" : []
+      }
+    },
+    "3" : {
+      "_id" : new ObjectID("000000000000000000000003"),
+      "nickname" : "r-man",
+      "fullName" : "Roman Ganchin",
+      "email" : "rganchin@umass.edu",
+      "location" : "Amherst, MA",
+      "feed": 3,
+      "productManager" : {
+        "_id" : 3,
+        "items" : []
+      },
+      "messages" : {
+        "_id" : 3,
+        "messages" : []
+      }
+    },
+    "4" : {
+      "_id" : new ObjectID("000000000000000000000004"),
+      "nickname" : "SuzyQ",
+      "fullName" : "Suzie Kim",
+      "email" : "gahyunkim@umass.edu",
+      "location" : "Amherst, MA",
+      "feed": 4,
+      "productManager" : {
+        "_id" : new ObjectID("000000000000000000000004"),
+        "items" : []
+      },
+      "messages" : {
+        "_id" : new ObjectID("000000000000000000000004"),
+        "messages" : []
+      }
+    },
+    "5" : {
+      "_id" : new ObjectID("000000000000000000000005"),
+      "username" : "DMix-Dogs",
+      "description" : "We are DMix's dogs.",
+      "languages" : "English",
+      "fullName" : "Cardie Duncan",
+      "email" : "cardieduncan@umass.edu",
+      "location" : "Amherst, MA",
+      "feed": 5,
+      "productManager" : {
+        "_id" : 5,
+        "items" : [2, 3, 4]
+      },
+      "messages" : {
+        "_id" : 5,
+        "messages": [1, 2, 3]
+      }
+    }
+  },
+
+  // FEEDS
+  "feeds" : {
+    "1" : {
+      "_id" : new ObjectID("000000000000000000000001"),
+      "items" : [2, 3, 4],
+      "categories" : ["Dorm","Snacks","Electronics"]
+    },
+    "2" : {
+      "_id" : new ObjectID("000000000000000000000002"),
+      "items" : [1, 2, 3, 4, 5, 6, 7, 8],
+      "categories" : ["Dorm","Snacks","Electronics"]
+    },
+    "3" : {
+      "_id" : new ObjectID("000000000000000000000003"),
+      "items" : [1, 2, 3, 4, 5, 6, 7, 8],
+      "categories" : ["Dorm","Snacks","Electronics"]
+    },
+    "4" : {
+      "_id" : new ObjectID("000000000000000000000004"),
+      "items" : [1, 2, 3, 4, 5, 6, 7, 8],
+      "categories" : ["Dorm","Snacks","Electronics"]
+    },
+    "5" : {
+      "_id" : new ObjectID("000000000000000000000005"),
+      "items" : [1, 5, 6, 7, 8],
+      "categories" : ["Dorm","Snacks","Electronics"]
+    }
+
+  },
+  "categories":{
+    "Dorm": {
+      "_id": "Dorm",
+      "items" : [1]
+    },
+    "Snacks":{
+      "_id": "Snacks",
+      "items": [2]
+    },
+    "Electronics":{
+      "_id": "Electronics",
+      "items":[3,4,5,6,7,8]
+    }
+  },
+  // ITEMS
+  "items" : {
+    "1" :{
+      "_id" : new ObjectID("000000000000000000000001"),
+      "name" : "Lamp",
+      "description" : "Nice lamp",
+      "category" : "Dorm",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : 1,
+      "vendorID" : 1
+    },
+    "2" :{
+      "_id" : new ObjectID("000000000000000000000002"),
+      "name" : "Dog Biscuit",
+      "description" : "Yummy",
+      "category" : "Snacks",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000002"),
+      "vendorID" : new ObjectID("000000000000000000000005")
+    },
+    "3" :{
+      "_id" : new ObjectID("000000000000000000000003"),
+      "name" : "Apple Lightning Charging Cable",
+      "description" : "Nylon Braided Cable",
+      "category" : "Electronics",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000003"),
+      "vendorID" : new ObjectID("000000000000000000000005")
+    },
+    "4" :{
+      "_id" : new ObjectID("000000000000000000000004"),
+      "name" : "Computer Mouse",
+      "description" : "Cool computer mouse",
+      "category" : "Electronics",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000004"),
+      "vendorID" : new ObjectID("000000000000000000000005")
+    },
+    "5" :{
+      "_id" : new ObjectID("000000000000000000000005"),
+      "name" : "Monitor",
+      "description" : "Next generation 20K monitor",
+      "category" : "Electronics",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000005"),
+      "vendorID" : new ObjectID("000000000000000000000001")
+    },
+    "6" :{
+      "_id" : new ObjectID("000000000000000000000006"),
+      "name" : "Dongle",
+      "description" : "You probably lost your last one",
+      "category" : "Electronics",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000006"),
+      "vendorID" : new ObjectID("000000000000000000000001")
+    },
+    "7" :{
+      "_id" : new ObjectID("000000000000000000000007"),
+      "name" : "Apple Watch",
+      "description" : "time at your convenience",
+      "category" : "Electronics",
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "http://placehold.it/400x300",
+      "photoID" : new ObjectID("000000000000000000000007"),
+      "vendorID" : new ObjectID("000000000000000000000001")
+    },
+    "8" :{
+      "_id" : new ObjectID("000000000000000000000008"),
+      "name" : "tiger",
+      "description" : "this should not be in Electronics",
+      "category" : "Electronics",
+      //stores all the users that liked the item
+      "likeCounter": [],
+      "dislikeCounter": [],
+      "contents" : "https://tse4.mm.bing.net/th?id=OIP.Mf749e3b566949daac2ba418a7e8fbbbbo0&pid=15.1",
+      "photoID" : new ObjectID("000000000000000000000008"),
+      "vendorID" : new ObjectID("000000000000000000000001")
+    }
+  },
+  // Messages
+  "messages" : {
+    "1" : {
+      "_id" : new ObjectID("000000000000000000000001"),
+      "sender" : new ObjectID("000000000000000000000001"),
+      "recipient" : new ObjectID("000000000000000000000005"),
+      "date" : "1/1/2016",
+      "contents" : "Hi, I would like to buy your tiger."
+    },
+    "2" : {
+      "_id" : new ObjectID("000000000000000000000002"),
+      "sender" : new ObjectID("000000000000000000000005"),
+      "recipient" : new ObjectID("000000000000000000000001"),
+      "date" : "1/1/2016",
+      "contents" : "I am willing to trade my tiger for your dongle."
+    },
+    "3" : {
+      "_id" : new ObjectID("000000000000000000000003"),
+      "sender" : new ObjectID("000000000000000000000001"),
+      "recipient" : new ObjectID("000000000000000000000005"),
+      "date" : "1/1/2016",
+      "contents" : "Ok I will meet you at the campus zoo."
+    }
+  }
+};
 
 /**
  * Resets a collection.
